0d8632e9c4415bf5f4edd3b67bf439fc0aa2fc0f
Nehemiah Yu <43020289+Nyu10@users.noreply.github.com>
2022-04-17 22:31:04 -0400

    daf

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from sklearn.cluster import KMeans
 import numpy as np
 import os
 
-
-#get all .edges files
 text_files = [file for file in os.listdir("facebook") if file.endswith('.edges')]
 for i in range(len(text_files)):
     text_files[i] =  "facebook/" + text_files[i]
-# print(text_files)
-
 adjacency_matrix = np.zeros((4039,4039))
 i = 0
 for file_names in text_files:
@@ -19,26 +15,19 @@ for file_names in text_files:
             line = line.split()
             adjacency_matrix[int(line[0])][int(line[1])] = 1
             adjacency_matrix[int(line[1])][int(line[0])] = 1
-
-# for i in range(len(adjacency_matrix)):
-#     for j in range(len(adjacency_matrix)):
-#         if (adjacency_matrix[i][j] == 1):
-#             print(i,j)
-
-# debug_output = open("debug_output.txt", "w")
-# for i in range(len(adjacency_matrix)):
-#     for j in range(len(adjacency_matrix)):
-#         debug_output.write(np.array2string(adjacency_matrix[i][j]) + " ")
-#     debug_output.write("\n")
-
-# ON POSTER
-# DEGREE MATRIX IS THE AMOUNT OF FRIENDS
 degree_matrix = np.diag(adjacency_matrix.sum(axis=1))
-# for i in range(len(degree_matrix)):
-#     print(degree_matrix[i][i])
 
 laplacian = degree_matrix - adjacency_matrix
-print(laplacian)
+
+def show_graph_with_labels(adjacency_matrix):
+    rows, cols = np.where(adjacency_matrix == 1)
+    edges = zip(rows.tolist(), cols.tolist())
+    gr = nx.Graph()
+    gr.add_edges_from(edges)
+    nx.draw_networkx_nodes(gr, pos=nx.spring_layout(gr),node_size=2, node_color=['blue'])
+    nx.draw_networkx_edges(gr, pos=nx.spring_layout(gr),width=0.1, alpha=0.5)
+    plt.show()
+show_graph_with_labels(adjacency_matrix)
 # eigenvalues and eigenvectors
 vals, vecs = np.linalg.eig(laplacian)
 
@@ -52,8 +41,3 @@ kmeans.fit(vecs[:,1:4])
 colors = kmeans.labels_
 
 print("Clusters:", colors)
-# debug_output = open("debug_output.txt", "w")
-# for i in range(len(laplacian)):
-#     for j in range(len(laplacian)):
-#         debug_output.write(np.array2string(laplacian[i][j]) + " ")
-#     debug_output.write("\n")
